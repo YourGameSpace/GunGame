1b32c8c1a4b16a773df93f7947764f759ccbd2e0
TUBEOF <46605849+YourGameSpace@users.noreply.github.com>
2021-07-14 00:54:02 +0200

    Added Main Command Interface

---
+++ src/main/java/com/yourgamespace/gungame/commands/GunGameCMD.java
@@ -0,0 +1,33 @@
+package com.yourgamespace.gungame.commands;
+
+import com.yourgamespace.gungame.data.Data;
+import com.yourgamespace.gungame.main.GunGame;
+import de.tubeof.tubetils.api.cache.CacheContainer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@SuppressWarnings("NullableProblems")
+public class GunGameCMD implements CommandExecutor {
+
+    private final Data data = GunGame.getData();
+    private final CacheContainer cacheContainer = GunGame.getCacheContainer();
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage(cacheContainer.get(String.class, "PREFIX") + "§cYou have to be a player!");
+            return true;
+        }
+        Player player = (Player) commandSender;
+
+
+
+        return true;
+    }
+
+    private void sendUsageMessage(Player player) {
+        //player.sendMessage("");
+    }
+}
