3f0ec0643f3cdc8fffc0a2dbea337ddf515e6fef
TUBEOF <46605849+YourGameSpace@users.noreply.github.com>
2021-07-15 22:32:35 +0200

    Added PacketSender

--- src/main/java/com/yourgamespace/gungame/listener/PlayerDeath.java
+++ src/main/java/com/yourgamespace/gungame/listener/PlayerDeath.java
@@ -2,6 +2,7 @@ package com.yourgamespace.gungame.listener;
 
 import com.yourgamespace.gungame.main.GunGame;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_14_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -27,6 +28,7 @@ public class PlayerDeath implements Listener {
         player.getInventory().clear();
         player.getInventory().setArmorContents(null);
         //For 1.13+
-        Bukkit.getScheduler().runTaskLaterAsynchronously(GunGame.getInstance(), () -> player.spigot().respawn(), 5);
+        Bukkit.getScheduler().runTaskLaterAsynchronously(GunGame.getInstance(), () -> ((CraftPlayer) player).getHandle().netServerHandler.a(new Packet9Respawn());, 5);
+        player.spigot().respawn();
     }
 }
---
+++ src/main/java/com/yourgamespace/gungame/utils/PacketSender.java
@@ -0,0 +1,30 @@
+package com.yourgamespace.gungame.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+@SuppressWarnings("ALL")
+public class PacketSender {
+
+    public PacketSender() {}
+
+    private Class<?> getNMS(String name) {
+        String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+        try {
+            return Class.forName("net.minecraft.server." + version + "." + name);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void sendPacket(Player player, Object packet) {
+        try {
+            Object handle = player.getClass().getMethod("getHandle", new Class[0]).invoke(player, new Object[0]);
+            Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
+            playerConnection.getClass().getMethod("sendPacket", new Class[] { getNMS("Packet") }).invoke(playerConnection, new Object[] { packet });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
